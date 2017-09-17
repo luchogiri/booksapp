ed7c6a1e7f7eaa7b0eac751eb113b2da8809070d
Lucho Giribone <lagiribone@gmail.com>
2017-09-16 22:33:20 -0300

    Crea los diferentes test:  que renderize, que disponga de cierta property, que el snapshot sea consistente

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,7 +1,9 @@
 import 'react-native';
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {Text} from 'react-native';
 
+import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 
 import Header from '../lib/components/header';
 
@@ -9,8 +11,29 @@ import Header from '../lib/components/header';
 
 describe('El componente Header', () => {
 
-  it('deberia ser renderizado', () => {
-    const component = renderer.create(<Header />);
+  describe('Al importarlo y utilizarlo', () => {
+    it('deberia ser renderizado sin problemas', () => {
+      const component = renderer.create(<Header />);
+    });
+  });
+
+  describe('Cuando lo instanciamos con prop "title"', () => {
+    it('deberia disponer de la propiedad "title" correcta', () => {
+      const component = shallow(<Header title="titulo" />);
+      // primero chequeamos que reciba dicha propiedad
+      expect( component.unrendered.props ).toHaveProperty("title", "titulo");
+      // luego buscamos el componente y chequeamos que tenga dicho valor
+      expect( component.find(Text).props() ).toHaveProperty("children", "titulo");
+    });
+  });
+
+  describe('Generando un snapshot de Header', () => {
+    it('deberia guardarse y mantener consistencia en diferentes configuraciones', () => {
+      const component = shallow(<Header />);
+      expect( component ).toMatchSnapshot();
+      component.setProps({ 'title': 'titulo' });
+      expect( component ).toMatchSnapshot();
+    });
   });
 
 });
